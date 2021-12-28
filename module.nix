6bd5fd1cca1ac58d038765e48d64a461a38e068c
NomisIV <simon@nomisiv.com>
2021-12-28 15:22:25 +0100

    add module

---
+++ module.nix
@@ -0,0 +1,67 @@
+{ lib, config, pkgs, ... }: let
+  cfg = config.services.servera;
+in {
+  options.services.fildela = with lib; {
+    enable = mkEnableOption "fildela HTTP file server";
+
+    openFirewall = mkEnableOption "open the port for servera in the firewall";
+
+    port = mkOption {
+      type = types.port;
+      default = 8000;
+      description = "The port to serve at";
+    };
+
+    root = mkOption {
+      type = types.str;
+      default = "/var/lib/fildela";
+      description = "The location of the file root";
+    };
+
+    user = mkOption {
+      type = types.str;
+      default = "fildela";
+      description = "The user to run fildela as";
+    };
+
+    group = mkOption {
+      type = types.str;
+      default = "fildela";
+      description = "The group to run fildela as";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Main servera service
+    systemd.services.fildela = {
+      description = "fildela HTTP file server";
+      after = [ "network.target" ];
+      requires = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        ExecStart = "${pkgs.fildela}/bin/fildela ${toString cfg.port} ${cfg.root}";
+        User = cfg.user;
+        Group = cfg.group;
+      };
+    };
+
+    # Open firewall
+    networking.firewall = lib.mkIf cfg.openFirewall {
+      allowedTCPPorts = [ cfg.port ];
+    };
+
+    # Add user and group
+    users.users = lib.mkIf (cfg.user == "fildela") {
+      servera = {
+        group = cfg.group;
+        home = cfg.dataDir;
+        uid = config.ids.uids.servera;
+      };
+    };
+
+    users.groups = lib.mkIf (cfg.group == "fildela") {
+      servera.gid = config.ids.gids.servera;
+    };
+  };
+}
